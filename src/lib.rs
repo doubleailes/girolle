4232cb3bf057d18458aa17b820628b0741b80c67
Philippe Llerena <philippe.llerena@forticheprod.com>
2023-12-20 07:25:13 +0100

    feat: add tokio impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use futures_lite::stream::StreamExt;
 use lapin::{
-    options::*, publisher_confirm::Confirmation, types::FieldTable, BasicProperties, Channel,
-    Connection, ConnectionProperties, Result,
+    message::DeliveryResult, options::*, publisher_confirm::Confirmation, types::FieldTable,
+    BasicProperties, Channel, Connection, ConnectionProperties, Result,
 };
 pub use serde_json as JsonValue;
 use serde_json::Value;
@@ -179,4 +179,154 @@ pub fn rpc_service(
         info!("GoodBye!");
         Ok(())
     })
+}
+
+#[tokio::main]
+pub async fn tokio_rpc_service(service_name: String, f: HashMap<String, fn(Vec<&Value>) -> Value>) {
+    // Set the log level
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    // Define the queue name1
+    let rpc_queue = format!("rpc-{}", service_name);
+    let routing_key = format!("{}.*", service_name);
+    // Add tracing
+    tracing_subscriber::fmt::init();
+    let addr = get_address();
+    // Uuid of the service
+    let id = Uuid::new_v4();
+
+    let options = ConnectionProperties::default()
+        // Use tokio executor and reactor.
+        // At the moment the reactor is only available for unix.
+        .with_executor(tokio_executor_trait::Tokio::current())
+        .with_reactor(tokio_reactor_trait::Tokio);
+
+    // Connect to RabbitMQ
+    let conn = Connection::connect(&addr, options).await.unwrap();
+    info!("CONNECTED");
+    // Open a channel and set the QOS
+    let incomming_channel = conn.create_channel().await.unwrap();
+    let response_channel = conn.create_channel().await.unwrap();
+
+    incomming_channel
+        .basic_qos(10, BasicQosOptions::default())
+        .await
+        .unwrap();
+    let mut queue_declare_options = QueueDeclareOptions::default();
+    queue_declare_options.durable = true;
+    let queue = incomming_channel
+        .queue_declare(&rpc_queue, queue_declare_options, FieldTable::default())
+        .await
+        .unwrap();
+    let _incomming_queue = incomming_channel
+        .queue_bind(
+            &rpc_queue,
+            "nameko-rpc",
+            &routing_key,
+            QueueBindOptions::default(),
+            FieldTable::default(),
+        )
+        .await
+        .unwrap();
+    // Declare the reply queue
+    let rpc_queue_reply = format!("rpc.reply-{}-{}", service_name, &id);
+    response_channel
+        .queue_declare(
+            &rpc_queue_reply,
+            queue_declare_options,
+            FieldTable::default(),
+        )
+        .await
+        .unwrap();
+    response_channel
+        .queue_bind(
+            &rpc_queue_reply,
+            "nameko-rpc",
+            &format!("{}", &id),
+            QueueBindOptions::default(),
+            FieldTable::default(),
+        )
+        .await
+        .unwrap();
+    info!(?queue, "Declared queue");
+    // Start a consumer.
+    let consumer = incomming_channel
+        .basic_consume(
+            &rpc_queue,
+            "my_consumer",
+            BasicConsumeOptions::default(),
+            FieldTable::default(),
+        )
+        .await
+        .unwrap();
+    consumer.set_delegate(move |delivery: DeliveryResult| {
+        let response_channel_clone = response_channel.clone();
+        let f_clone = f.clone();
+        let rpc_queue_reply_clone = rpc_queue_reply.clone();
+        async move {
+            let delivery = match delivery {
+                // Carries the delivery alongside its channel
+                Ok(Some(delivery)) => delivery,
+                // The consumer got canceled
+                Ok(None) => return,
+                // Carries the error and is always followed by Ok(None)
+                Err(error) => {
+                    dbg!("Failed to consume queue message {}", error);
+                    return;
+                }
+            };
+
+            let opt_routing_key = delivery.routing_key.to_string();
+            let fn_service: fn(Vec<&Value>) -> Value = match f_clone.get(&opt_routing_key) {
+                Some(fn_service) => *fn_service,
+                None => {
+                    warn!("fn_service: None");
+                    return;
+                }
+            };
+            delivery.ack(BasicAckOptions::default()).await.expect("ack");
+            let incomming_data: Value = serde_json::from_slice(&delivery.data).expect("json");
+            let args: Vec<&Value> = incomming_data["args"]
+                .as_array()
+                .expect("args")
+                .iter()
+                .collect();
+            // Get the correlation_id and reply_to_id
+            let opt_correlation_id = delivery.properties.correlation_id();
+            let correlation_id = match opt_correlation_id {
+                Some(correlation_id) => correlation_id.to_string(),
+                None => {
+                    error!("correlation_id: None");
+                    panic!("correlation_id: None")
+                }
+            };
+            let opt_reply_to_id = delivery.properties.reply_to();
+            let reply_to_id = match opt_reply_to_id {
+                Some(reply_to_id) => reply_to_id.to_string(),
+                None => {
+                    error!("reply_to_id: None");
+                    panic!("reply_to_id: None")
+                }
+            };
+            let properties = BasicProperties::default()
+                .with_correlation_id(correlation_id.into())
+                .with_content_type("application/json".into())
+                .with_reply_to(rpc_queue_reply_clone.clone().into());
+
+            // Publish the response
+            let payload: String = json!(
+                {
+                    "result": fn_service(args),
+                    "error": null,
+                }
+            )
+            .to_string();
+            publish(&response_channel_clone, payload, properties, reply_to_id)
+                .await
+                .expect("Error publishing");
+        }
+    });
+
+    std::future::pending::<()>().await;
 }
