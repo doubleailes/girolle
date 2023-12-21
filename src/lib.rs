0802408aae1cb90d9cff8cf3cafd6ff69e8f2c63
Philippe Llerena <philippe.llerena@forticheprod.com>
2023-12-21 17:21:03 +0100

    Refactor variable names for clarity and consistency

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@
 ///
 use futures_lite::stream::StreamExt;
 use lapin::{
-    message::DeliveryResult, options::*, publisher_confirm::Confirmation, types::FieldTable,
+    message::{DeliveryResult, Delivery}, options::*, publisher_confirm::Confirmation, types::FieldTable,
     BasicProperties, Channel, Connection, ConnectionProperties, Result,
 };
 pub use serde_json as JsonValue;
@@ -58,11 +58,11 @@ impl RpcService {
         rpc_service(self.service_name.clone(), self.f.clone())
     }
     // Start the RPC service with tokio runtime
-    pub fn start_tokio(&self) {
+    pub fn start_tokio(&self) -> Result<()> {
         if self.f.is_empty() {
             panic!("No function insert");
         }
-        tokio_rpc_service(self.service_name.clone(), self.f.clone());
+        tokio_rpc_service(self.service_name.clone(), self.f.clone())
     }
     // Get the list of the routing keys
     pub fn get_routing_keys(&self) -> Vec<String> {
@@ -98,97 +98,61 @@ async fn publish(
     Ok(confirm)
 }
 
-/// Create RPC service and listen to the Nameko RPC queue for the service_name
-/// The service_name is the name of the service in the Nameko microservice
-/// The f is a HashMap of the functions that will be called when the routing key is called
-/// The function f must return a serializable value
-fn rpc_service(service_name: String, f: HashMap<String, fn(Vec<&Value>) -> Value>) -> Result<()> {
-    const PREFETCH_COUNT: u16 = 10;
+/// Setup the queues for the RPC service
+async fn setup_queues(
+    service_name: &String,
+    id: &Uuid,
+    options: ConnectionProperties,
+) -> Result<(lapin::Channel, lapin::Channel,std::string::String)> {
+    let addr = get_address();
+    // Connect to RabbitMQ
+    let conn = Connection::connect(&addr, options).await?;
+    info!("CONNECTED");
+    // Open a channel and set the QOS
+    let incomming_channel = conn.create_channel().await?;
+    let response_channel = conn.create_channel().await?;
     const QUEUE_TTL: u32 = 300000;
-    // Set the log level
-    if std::env::var("RUST_LOG").is_err() {
-        std::env::set_var("RUST_LOG", "info");
-    }
-    // Define the queue name1
     let rpc_queue = format!("rpc-{}", service_name);
     let routing_key = format!("{}.*", service_name);
-    // Add tracing
-    tracing_subscriber::fmt::init();
-    let addr = get_address();
-    // Uuid of the service
-    let id = Uuid::new_v4();
-    // check list of function
-    info!("List of functions {:?}", f.keys());
+    let mut queue_declare_options = QueueDeclareOptions::default();
+    queue_declare_options.durable = true;
+    let queue = incomming_channel
+        .queue_declare(&rpc_queue, queue_declare_options, FieldTable::default())
+        .await?;
+    let _incomming_queue = incomming_channel
+        .queue_bind(
+            &rpc_queue,
+            "nameko-rpc",
+            &routing_key,
+            QueueBindOptions::default(),
+            FieldTable::default(),
+        )
+        .await?;
+    info!(?queue, "Declared queue");
+    // Declare the reply queue
+    let mut response_arguments = FieldTable::default();
+    response_arguments.insert("x-expires".into(), QUEUE_TTL.into());
+    let rpc_queue_reply = format!("rpc.reply-{}-{}", service_name, &id);
+    response_channel
+        .queue_declare(&rpc_queue_reply, queue_declare_options, response_arguments)
+        .await?;
+    response_channel
+        .queue_bind(
+            &rpc_queue_reply,
+            "nameko-rpc",
+            &format!("{}", &id),
+            QueueBindOptions::default(),
+            FieldTable::default(),
+        )
+        .await?;
+    Ok((incomming_channel, response_channel, rpc_queue_reply))
+}
 
-    async_global_executor::block_on(async {
-        // Connect to RabbitMQ
-        let conn = Connection::connect(&addr, ConnectionProperties::default()).await?;
-        info!("CONNECTED");
-        // Open a channel and set the QOS
-        let incomming_channel = conn.create_channel().await?;
-        let response_channel = conn.create_channel().await?;
-        incomming_channel
-            .basic_qos(PREFETCH_COUNT, BasicQosOptions::default())
-            .await?;
-        let mut queue_declare_options = QueueDeclareOptions::default();
-        queue_declare_options.durable = true;
-        let queue = incomming_channel
-            .queue_declare(&rpc_queue, queue_declare_options, FieldTable::default())
-            .await?;
-        let _incomming_queue = incomming_channel
-            .queue_bind(
-                &rpc_queue,
-                "nameko-rpc",
-                &routing_key,
-                QueueBindOptions::default(),
-                FieldTable::default(),
-            )
-            .await
-            .unwrap();
-        // Declare the reply queue
-        let rpc_queue_reply = format!("rpc.reply-{}-{}", service_name, &id);
-        let mut response_arguments = FieldTable::default();
-        response_arguments.insert("x-expires".into(), QUEUE_TTL.into());
-
-        response_channel
-            .queue_declare(&rpc_queue_reply, queue_declare_options, response_arguments)
-            .await
-            .unwrap();
-        response_channel
-            .queue_bind(
-                &rpc_queue_reply,
-                "nameko-rpc",
-                &format!("{}", &id),
-                QueueBindOptions::default(),
-                FieldTable::default(),
-            )
-            .await
-            .unwrap();
-        info!(?queue, "Declared queue");
-        // Start a consumer.
-        let mut consumer = incomming_channel
-            .basic_consume(
-                &rpc_queue,
-                "my_consumer",
-                BasicConsumeOptions::default(),
-                FieldTable::default(),
-            )
-            .await?;
-        info!("will consume");
-        // Iterate over deliveries.
-        while let Some(delivery) = consumer.next().await {
-            let delivery = delivery.expect("error in consumer");
-            let opt_routing_key = delivery.routing_key.to_string();
-            let fn_service: fn(Vec<&Value>) -> Value = match f.get(&opt_routing_key) {
-                Some(fn_service) => *fn_service,
-                None => {
-                    warn!("fn_service: {} not found", &opt_routing_key);
-                    continue;
-                }
-            };
-            delivery.ack(BasicAckOptions::default()).await.expect("ack");
-            let incomming_data: Value = serde_json::from_slice(&delivery.data).expect("json");
-            let args: Vec<&Value> = incomming_data["args"]
+/// Execute the delivery
+async fn execute_delivery(delivery: Delivery, id: &Uuid, fn_service: fn(Vec<&Value>) -> Value, response_channel: &Channel, rpc_queue_reply: &String){
+    let opt_routing_key = delivery.routing_key.to_string();
+    let incomming_data: Value = serde_json::from_slice(&delivery.data).expect("json");
+    let args: Vec<&Value> = incomming_data["args"]
                 .as_array()
                 .expect("args")
                 .iter()
@@ -219,6 +183,48 @@ fn rpc_service(service_name: String, f: HashMap<String, fn(Vec<&Value>) -> Value
             publish(&response_channel, payload, properties, reply_to_id)
                 .await
                 .expect("Error publishing");
+}
+
+/// Create RPC service and listen to the Nameko RPC queue for the service_name
+/// The service_name is the name of the service in the Nameko microservice
+/// The f is a HashMap of the functions that will be called when the routing key is called
+/// The function f must return a serializable value
+fn rpc_service(service_name: String, f: HashMap<String, fn(Vec<&Value>) -> Value>) -> Result<()> {
+    // Define the queue name1
+    let rpc_queue = format!("rpc-{}", service_name);
+    // Add tracing
+    tracing_subscriber::fmt::init();
+    // Uuid of the service
+    let id = Uuid::new_v4();
+    // check list of function
+    dbg!("List of functions {:?}", f.keys());
+
+    async_global_executor::block_on(async {
+        let (incomming_channel, response_channel,rpc_queue_reply) = setup_queues(&service_name, &id, ConnectionProperties::default()).await?;
+        let rpc_queue_reply = format!("rpc.reply-{}-{}", service_name, &id);
+        // Start a consumer.
+        let mut consumer = incomming_channel
+            .basic_consume(
+                &rpc_queue,
+                "my_consumer",
+                BasicConsumeOptions::default(),
+                FieldTable::default(),
+            )
+            .await?;
+        info!("will consume");
+        // Iterate over deliveries.
+        while let Some(delivery) = consumer.next().await {
+            let delivery = delivery.expect("error in consumer");
+            let opt_routing_key = delivery.routing_key.to_string();
+            let fn_service: fn(Vec<&Value>) -> Value = match f.get(&opt_routing_key) {
+                Some(fn_service) => *fn_service,
+                None => {
+                    warn!("fn_service: {} not found", &opt_routing_key);
+                    continue;
+                }
+            };
+            delivery.ack(BasicAckOptions::default()).await.expect("ack");
+            execute_delivery(delivery, &id, fn_service, &response_channel, &rpc_queue_reply).await;
         }
         info!("GoodBye!");
         Ok(())
@@ -226,19 +232,16 @@ fn rpc_service(service_name: String, f: HashMap<String, fn(Vec<&Value>) -> Value
 }
 
 #[tokio::main]
-pub async fn tokio_rpc_service(service_name: String, f: HashMap<String, fn(Vec<&Value>) -> Value>) {
-    const PREFETCH_COUNT: u16 = 10;
-    const QUEUE_TTL: u32 = 300000;
+pub async fn tokio_rpc_service(service_name: String, f: HashMap<String, fn(Vec<&Value>) -> Value>) -> Result<()> {
     // Set the log level
     if std::env::var("RUST_LOG").is_err() {
         std::env::set_var("RUST_LOG", "info");
     }
     // Define the queue name1
     let rpc_queue = format!("rpc-{}", service_name);
-    let routing_key = format!("{}.*", service_name);
     // Add tracing
     tracing_subscriber::fmt::init();
-    let addr = get_address();
+
     // Uuid of the service
     let id = Uuid::new_v4();
 
@@ -249,51 +252,7 @@ pub async fn tokio_rpc_service(service_name: String, f: HashMap<String, fn(Vec<&
         .with_reactor(tokio_reactor_trait::Tokio);
 
     // Connect to RabbitMQ
-    let conn = Connection::connect(&addr, options).await.unwrap();
-    info!("CONNECTED");
-    // Open a channel and set the QOS
-    let incomming_channel = conn.create_channel().await.unwrap();
-    let response_channel = conn.create_channel().await.unwrap();
-
-    incomming_channel
-        .basic_qos(PREFETCH_COUNT, BasicQosOptions::default())
-        .await
-        .unwrap();
-    let mut queue_declare_options = QueueDeclareOptions::default();
-    queue_declare_options.durable = true;
-    let queue = incomming_channel
-        .queue_declare(&rpc_queue, queue_declare_options, FieldTable::default())
-        .await
-        .unwrap();
-    let _incomming_queue = incomming_channel
-        .queue_bind(
-            &rpc_queue,
-            "nameko-rpc",
-            &routing_key,
-            QueueBindOptions::default(),
-            FieldTable::default(),
-        )
-        .await
-        .unwrap();
-    // Declare the reply queue
-    let rpc_queue_reply = format!("rpc.reply-{}-{}", service_name, &id);
-    let mut response_arguments = FieldTable::default();
-    response_arguments.insert("x-expires".into(), QUEUE_TTL.into());
-    response_channel
-        .queue_declare(&rpc_queue_reply, queue_declare_options, response_arguments)
-        .await
-        .unwrap();
-    response_channel
-        .queue_bind(
-            &rpc_queue_reply,
-            "nameko-rpc",
-            &format!("{}", &id),
-            QueueBindOptions::default(),
-            FieldTable::default(),
-        )
-        .await
-        .unwrap();
-    info!(?queue, "Declared queue");
+    let (incomming_channel, response_channel, rpc_queue_reply) = setup_queues(&service_name, &id, options).await?;
     // Start a consumer.
     let consumer = incomming_channel
         .basic_consume(
@@ -330,39 +289,10 @@ pub async fn tokio_rpc_service(service_name: String, f: HashMap<String, fn(Vec<&
                 }
             };
             delivery.ack(BasicAckOptions::default()).await.expect("ack");
-            let incomming_data: Value = serde_json::from_slice(&delivery.data).expect("json");
-            let args: Vec<&Value> = incomming_data["args"]
-                .as_array()
-                .expect("args")
-                .iter()
-                .collect();
-            // Get the correlation_id and reply_to_id
-            let correlation_id = get_id(delivery.properties.correlation_id(), "correlation_id");
-            let reply_to_id = get_id(delivery.properties.reply_to(), "reply_to_id");
-            let opt_headers = delivery.properties.headers();
-            let headers = insert_new_id_to_call_id(
-                opt_headers.as_ref().expect("headers").clone(),
-                &opt_routing_key,
-                &id.to_string(),
-            );
-            let properties = BasicProperties::default()
-                .with_correlation_id(correlation_id.into())
-                .with_content_type("application/json".into())
-                .with_reply_to(rpc_queue_reply_clone.into())
-                .with_headers(headers);
-            // Publish the response
-            let payload: String = json!(
-                {
-                    "result": fn_service(args),
-                    "error": null,
-                }
-            )
-            .to_string();
-            publish(&response_channel_clone, payload, properties, reply_to_id)
-                .await
-                .expect("Error publishing");
+            execute_delivery(delivery, &id, fn_service, &response_channel_clone, &rpc_queue_reply_clone).await;
         }
     });
 
     std::future::pending::<()>().await;
+    Ok(())
 }
