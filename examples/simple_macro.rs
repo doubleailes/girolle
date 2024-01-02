933ad521e365657921ae0d45a432a796d55c5671
Philippe Llerena <philippe.llerena@forticheprod.com>
2024-01-02 17:24:16 +0100

    Add fibonacci function and update dependencies

--- examples/simple_macro.rs
+++ examples/simple_macro.rs
@@ -4,13 +4,26 @@ use girolle_macro::girolle;
 
 #[girolle]
 fn hello(s: String) -> String {
-    let n: String = serde_json::from_value(data[0].clone()).unwrap();
-    let out: Value = serde_json::to_value(format!("Hello, {}!", n)).unwrap();
-    Ok(out)
+    format!("Hello, {}!", s)
+}
+
+fn fibonacci(n: u64) -> u64 {
+    if n <= 1 {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+#[girolle]
+fn fib_warp(n: u64) -> u64 {
+    fibonacci(n)
 }
 
 fn main() {
-    let binding = serde_json::to_value("World".to_string()).unwrap();
-    let s = vec![&binding];
-    let _ = hello(s);
+    let rpc_task = girolle::RpcTask::new("hello", hello);
+    let rpc_task_fib = girolle::RpcTask::new("fibonacci", fib_warp);
+    let _ = girolle::RpcService::new("video")
+        .register(rpc_task)
+        .register(rpc_task_fib)
+        .start();
 }
--- girolle_macro/src/entry.rs
+++ girolle_macro/src/entry.rs
@@ -8,10 +8,52 @@ use syn::{parse2, FnArg, ItemFn};
 
 struct Task {
     args: Vec<FnArg>,
+    inner_statements: Vec<syn::Stmt>,
 }
 impl Task {
     fn new() -> Self {
-        Task { args: Vec::new() }
+        Task {
+            args: Vec::new(),
+            inner_statements: Vec::new(),
+        }
+    }
+    fn add_input_serialize(&mut self) {
+        let mut stmts: Vec<syn::Stmt> = Vec::new();
+        let mut i: usize = 0;
+        for arg in &self.args {
+            let data_quote = quote! {
+                data[#i]
+            };
+            match arg {
+                FnArg::Typed(pat_type) => {
+                    let pat = &pat_type.pat;
+                    let ty = &pat_type.ty;
+                    stmts.push(
+                        parse_quote! {let #pat: #ty = serde_json::from_value(#data_quote.clone())?;},
+                    );
+                }
+                _ => {}
+            }
+            i += 1;
+        }
+        let previous_stmts = self.inner_statements.clone();
+        stmts.extend(previous_stmts.clone());
+        self.inner_statements = stmts.clone();
+    }
+    fn add_output_serialize(&mut self) {
+        let last_imut = self.inner_statements.pop().clone();
+        let output_quote: syn::Stmt = parse_quote! {let output = #last_imut;};
+        self.inner_statements.push(output_quote);
+    }
+    fn add_output_final(&mut self) {
+        let final_line: syn::Stmt = parse_quote! {return Ok(serde_json::to_value(output)?);};
+        self.inner_statements.push(final_line);
+    }
+    #[allow(dead_code)]
+    fn print_stmts(&self) {
+        for stmt in &self.inner_statements {
+            println!("{}", quote!(#stmt).to_string());
+        }
     }
 }
 
@@ -25,9 +67,14 @@ impl Fold for Task {
             data: Vec<&Value>
         };
         // Replace the return type by the NamekoFunction return type
-        folded_item.output = parse_quote! {
-            -> girolle::Result<Value>
-        };
+        folded_item.output = parse_quote! {-> girolle::Result<Value>};
+        folded_item
+    }
+    fn fold_stmt(&mut self, i: syn::Stmt) -> syn::Stmt {
+        let folded_item = i.clone();
+        // Capture the original statements
+        self.inner_statements.push(folded_item.clone());
+        // Replace the statements by the NamekoFunction statements
         folded_item
     }
 }
@@ -39,8 +86,10 @@ pub(crate) fn main(input: TokenStream) -> TokenStream {
         Err(error) => return error.to_compile_error(),
     };
     let mut task = Task::new();
-    let new_item_fn = task.fold_item_fn(old_item_fn);
-    println!("numbers of args {}", task.args.len());
-    println!("{:?}", quote!(#new_item_fn).to_string());
-    quote!(#new_item_fn)
+    let mut new_item_fn = task.fold_item_fn(old_item_fn);
+    task.add_input_serialize();
+    task.add_output_serialize();
+    task.add_output_final();
+    new_item_fn.block.stmts = task.inner_statements.clone();
+    TokenStream::from(quote!(#new_item_fn))
 }
