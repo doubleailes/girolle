da406d53754a255e52dee1c513efbf6dd55b8fa4
Philippe Llerena <philippe.llerena@forticheprod.com>
2024-01-22 16:11:00 +0100

    Refactor imports in examples

--- examples/simple_service.rs
+++ examples/simple_service.rs
@@ -1,4 +1,5 @@
-use girolle::{JsonValue::Value, Result, RpcService, RpcTask};
+use girolle::{Result, RpcService, RpcTask};
+use girolle::prelude::*;
 use serde_json;
 
 fn hello(s: Vec<&Value>) -> Result<Value> {
