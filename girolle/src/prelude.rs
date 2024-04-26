24f1edc395b25a523c2e5448e5e6a5f318080b7e
Philippe Llerena <philippe.llerena@forticheprod.com>
2024-04-26 16:36:28 +0200

    Update dependencies and add RpcService and RpcTask to prelude.rs

--- girolle/src/prelude.rs
+++ girolle/src/prelude.rs
@@ -1,4 +1,5 @@
+pub use crate::{RpcService, RpcTask};
 ///! This module contains the most commonly used types, functions and macros.
 pub use girolle_macro::girolle;
+pub use serde_json;
 pub use serde_json::{json, Value};
-pub use crate::{RpcService, RpcTask};
