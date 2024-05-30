64dbab113805a4c5c4e7daaefe2b2ad3d52a734c
Philippe Llerena <philippe.llerena@gmail.com>
2024-05-30 15:57:07 +0200

    feat: Update Config::with_yaml_defaults to accept a String parameter
    The code changes modify the `Config::with_yaml_defaults` function in multiple files to accept a `String` parameter instead of a string slice. This change allows for more flexibility in specifying the YAML configuration file path.
    
    Recent user commits and repository commits indicate a pattern of updating dependencies, improving documentation, and adding new features.

--- examples/simple_macro.rs
+++ examples/simple_macro.rs
@@ -25,7 +25,7 @@ fn fib_warp(n: u64) -> u64 {
 }
 
 fn main() {
-    let conf: Config = Config::with_yaml_defaults("staging/config.yml").unwrap();
+    let conf: Config = Config::with_yaml_defaults("staging/config.yml".to_string()).unwrap();
     let rpc_task = RpcTask::new("hello", hello);
     let rpc_task_fib = RpcTask::new("fibonacci", fib_warp);
     let rpc_task_sleep = RpcTask::new("sleep", temporary_sleep);
